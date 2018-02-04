6e4a64eb73b41696de0d054296c0a61384fb1c61
Mahendra Yadav <mahendra@azrisolutions.com>
2018-02-04 17:41:05 +0530

    Tutorial Part 5: Creating our home page

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -35,4 +35,4 @@ urlpatterns += [
     path('', RedirectView.as_view(url='/catalog/')),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
