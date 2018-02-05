77e73d7fc8ceeea632260cdfd3b23507eb587ef2
Mahendra Yadav <mahendra@azrisolutions.com>
2018-02-05 22:12:58 +0530

    Tutorial Part 8: - Challenge yourself code

--- catalog/urls.py
+++ catalog/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
 
 urlpatterns += [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('borrowed/', views.ManageLoanedBooksListView.as_view(), name='all-borrowed'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 from .models import Book, Author, BookInstance  # , Genre
 
@@ -63,3 +63,13 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+
+class ManageLoanedBooksListView(PermissionRequiredMixin, generic.ListView):
+    permission_required = 'catalog.can_mark_returned'
+    template_name = 'catalog/list_all_borrowed_books.html'
+    model = BookInstance
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
